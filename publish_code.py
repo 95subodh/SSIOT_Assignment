9d3b1d358a0fdd9f0c444766c4d3e8885aaf7114
Subodh Yadav <subodh29031995@gmail.com>
2016-11-30 18:29:55 +0530

    publishing code for aws-iot

---
+++ publish_code.py
@@ -0,0 +1,66 @@
+import os
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import sys
+import logging
+import time
+import getopt
+
+# Custom MQTT message callback
+def customCallback(client, userdata, message):
+	print("Received a new message: ")
+	print(message.payload)
+	print("from topic: ")
+	print(message.topic)
+	print("--------------\n\n")
+
+# Read in command-line parameters
+useWebsocket = False
+host = "a3r63vy9vas8ta.iot.us-east-1.amazonaws.com"
+rootCAPath = "./certs/rootCA.pem.crt"
+certificatePath = "./certs/6fe0c03a66-certificate.pem.crt"
+privateKeyPath = "./certs/6fe0c03a66-private.pem.key"
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+if useWebsocket:
+	myAWSIoTMQTTClient = AWSIoTMQTTClient("basicPubSub", useWebsocket=True)
+	myAWSIoTMQTTClient.configureEndpoint(host, 443)
+	myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+else:
+	myAWSIoTMQTTClient = AWSIoTMQTTClient("basicPubSub")
+	myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+	myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect and subscribe to AWS IoT
+topic = "sdk/test/Python"
+myAWSIoTMQTTClient.connect()
+myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
+time.sleep(2)
+
+# Publish to the same topic in a loop forever
+loopCount = 0
+sensor_sn='00000001'
+try:
+	while True:
+		msg = '"Device": "{:s}", "Temperature": "{}", "Loop": "{}"'.format(sensor_sn, loopCount%43,loopCount)
+		msg = '{'+msg+'}'
+		myAWSIoTMQTTClient.publish(topic, msg, 1)
+		loopCount += 1
+		time.sleep(2)
+except KeyboardInterrupt:
+	pass
